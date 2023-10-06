d142a36860903b1bd1018f7256262471fd480dab
RIGX <dnxrd01@gmail.com>
2023-10-06 15:35:49 +0700

    Final Website and Coming Soon

--- Index.js
+++ Index.js
@@ -1,11 +1,21 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
+const wrapper_box = document.querySelector('.wrapper-box');
+const login_Link = document.querySelector('.login-link');
+const register_Link = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btn-Popup');
+const iconClose = document.querySelector('.icon-close');
 
-registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active');
+register_Link.addEventListener('click', () => {
+  wrapper_box.classList.add('active');
 });
 
-loginLink.addEventListener('onClick', () => {
-  wrapper.classList.remove('active');
+login_Link.addEventListener('click', () => {
+  wrapper_box.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+  wrapper_box.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+  wrapper_box.classList.remove('active-popup');
 });
